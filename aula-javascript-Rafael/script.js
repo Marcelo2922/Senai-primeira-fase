3e5b8aca48d03e30e913bc8a47b4d8b0dc65e3f5
Marcelo2922 <Marceloaugusto2922@gmail.com>
2025-09-04 21:49:49 -0300

    Teste

---
+++ aula-javascript-Rafael/script.js
@@ -0,0 +1,15 @@
+// alert("Olá mundo")
+// alert("Tudo bem?")
+// alert("Esta sozinho?")
+// alert("Então quem é esse atrás de você?")
+
+let idade 
+// idade = 18
+// alert(idade)
+// idade = "dezoito"
+// alert (idade)
+// idade = 18.5
+// alert (idade)
+const amor = "Meu primeiro amor "
+alert (amor)
+var antigao = "Nunca usar essa poioca"
