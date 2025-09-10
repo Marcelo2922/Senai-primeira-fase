9913cc0328375491329f3af34c0b8c251e30586b
Marcelo2922 <Marceloaugusto2922@gmail.com>
2025-09-09 22:12:22 -0300

    Aula de javascript

---
+++ logica-js/variaveis.js
@@ -0,0 +1,17 @@
+//Variavel declarada com var
+// var nome = "Marcelo"
+// var nome = "Marcos"
+//Variavel declarada com let
+// let nome = "Marcelo"
+// nome = "Marcos"
+//Variavel declarada com const
+const nome = "Marcelo" // string
+const sobrenome = "Oliveira"
+const idade = 21 // number
+const masculino = true //boolean
+const nome_completo = nome +  " " + sobrenome
+// Array
+const frutas = ["laranja","maçã","banana"]
+console.log(frutas[0])
+// console.log("Meu nome é ", nome_completo, " e a minha idade é ", idade, " anos")
+// console.log(`Meu nome é ${nome} ${sobrenome} e a minha idade é ${idade} anos `)
