1fab7f76716c63042c425fad0181030848eb8367
Marcelo2922 <Marceloaugusto2922@gmail.com>
2025-09-11 20:50:54 -0300

    Exercicios

--- aula-de-if-else/ex02.js
+++ aula-de-if-else/ex02.js
@@ -1,3 +1,3 @@
 let dias = prompt ("Digite um numero de 1 a 7: ")
-const dias_semana = [" ","domingo","segunda","terça","quarta","quinta","sexta","sabado"]
+const dias_semana = [" ","Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sabado"]
 alert(dias_semana[dias])
