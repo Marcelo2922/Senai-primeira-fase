c2e1e154aea3a411f32a59f052213b28b2cb975b
Marcelo2922 <Marceloaugusto2922@gmail.com>
2025-09-15 20:58:42 -0300

    Exercicio

--- aula-de-if-else/ex07.js
+++ aula-de-if-else/ex07.js
@@ -1,23 +1,14 @@
-function classificarnota (nota){
-    if (nota >= 90 && nota <=100){
-        return'A';
-    }else if (nota >=80){
-        return 'B'
-    }else if  (nota >=70){
-        return 'C'
-    }else if (nota >=50){
-        return 'D'
-    }else if (nota >= 0){
-        return 'E'
-    }else{
-        return 'Nota invalida'
-    }
-}
-let nota_do_aluno = prompt("Digite a nota do aluno: ")
-let nota = Number(nota_do_aluno)
-if (!isNaN(nota) && nota >= 0 && nota <= 100) {
-  let classificacao = classificarNota(nota);
-  alert(`Sua nota foi ${nota}. Classificação: ${classificacao}`);
-} else {
-  alert("Por favor, insira uma nota válida entre 0 e 100.");
+let nota = prompt("Digite sua nota: ")
+if (nota >=90) {
+    alert("Sua nota é A")
+}else if (nota >= 80){
+    alert("Sua nota é B")
+}else if (nota >= 60){
+    alert("Sua nota é C")
+}else if (nota>= 50){
+    alert("Sua nota é D")
+}else if (nota>=0){
+    alert("Sua nota é E")
+}else {
+    alert("Nota invalida!!!")
 }
