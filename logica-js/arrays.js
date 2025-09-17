4aa4d87a16080fa412a9538fe09d23c9f642e512
Marcelo2922 <Marceloaugusto2922@gmail.com>
2025-09-16 21:06:17 -0300

    Aula de js

---
+++ logica-js/arrays.js
@@ -0,0 +1,6 @@
+const alunos = ["Marcelo","Ana carolini de melo","Mauricio","Vera","Daniel","Leonardo"]
+console.log(`O nome da minha namorada é ${alunos[1]}`)
+
+const valores = [100, 100.20, 200.30]
+
+console.log(`A soma dos valeres é ${valores[1]+valores[2]}`)
---
+++ logica-js/objetos.js
@@ -0,0 +1,9 @@
+const usuario = {
+    login:"username", 
+    email:"email@gmail.com",
+    saldo:50.230,
+    islogin:true,
+    senha:"123456789"
+}
+
+console.log(usuario.login)
