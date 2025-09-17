aef96b57d602eb653821811539d3168678be4f3d
Marcelo2922 <Marceloaugusto2922@gmail.com>
2025-09-16 21:49:09 -0300

    Exercicios

---
+++ logica-js/arrays-objs.js
@@ -0,0 +1,14 @@
+const alunos_do_senai= [{
+    nome:"MArcelo",
+    email:"Marcelo@gmail.com",
+    matricula:"321",
+    idade:21
+},
+{
+    nome:"Ana",
+    email:"Anaamelhor@gmail.com",
+    matricula:"123",
+    idade:22
+}
+]
+console.log(`O nome da aluna é ${alunos_do_senai[1].nome}`)
