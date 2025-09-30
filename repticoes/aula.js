6561a02562ee7aaae226b25233acb7e35f2323b9
Marcelo2922 <Marceloaugusto2922@gmail.com>
2025-09-29 22:08:36 -0300

    Repetição

---
+++ repticoes/aula.js
@@ -0,0 +1,12 @@
+// let acertos = Number(prompt("Digite a quantidade acertos: "))
+// let valor_por_questao = 10/16
+// let nota = acertos * valor_por_questao
+
+// alert(`A sua nota é ${nota}`)
+
+let contador = 0
+
+while(contador < 5){
+   alert("oi")
+    contador =contador + 1
+}
