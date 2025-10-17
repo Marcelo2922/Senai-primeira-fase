6a544afaa835cf2adf4349953b56ce70c837a82e
Marcelo2922 <Marceloaugusto2922@gmail.com>
2025-10-16 21:54:25 -0300

    Exercicio

--- repticoes/exercicio-rep03.js
+++ repticoes/exercicio-rep03.js
@@ -1,5 +1,7 @@
 // 🐱‍🏍Calcule a soma dos números de 1 a 100.
+let soma= 0
 for (let i=0; i <=100; i++){
-    let soma = i+i
-    console.log(`Total Número: ${soma}`)
+    soma = i+i
+   
 }
+ console.log(`Total Número: ${soma}`)
